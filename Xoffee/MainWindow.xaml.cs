3f65945bd62e5e5e9a54471743c746c57289d65c
Ria Cherry <lilyflowerangel@gmail.com>
2022-12-27 02:11:04 +0530

    Add project files.

---
+++ Xoffee/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+
+namespace Xoffee;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Go_Click(object sender, RoutedEventArgs e)
+    {
+        LoadThis(AddressText.Text);
+    }
+
+    private void Home_Click(object sender, RoutedEventArgs e)
+    {
+        LoadThis("./www/Home.xaml");
+    }
+
+    private void LoadThis(string path)
+    {
+        using FileStream fs = new(path, FileMode.Open);
+        var page = XamlReader.Load(fs);
+        ContentSpace.Content = page as Page;
+    }
+}
